3daa2f9573c8dd24756e51112741c6c029be0b2c
Ahmed ElShahawy <ahmedsstem@gmail.com>
2022-08-14 01:42:39 +0200

    Simple App

---
+++ Client/Client1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress hostIP = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endPoint = new IPEndPoint(hostIP, 8000);
+            Socket socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socketClient.Connect(endPoint);
+            string S = "";
+            while (!S.Contains(","))
+            {
+                byte[] byted = new byte[1024];
+                int length = socketClient.Receive(byted);
+                int lengthOfFile = BitConverter.ToInt32(byted, 0);
+                string fileName = Encoding.ASCII.GetString(byted, 4, lengthOfFile);
+                S = Encoding.ASCII.GetString(byted, lengthOfFile + 4, 50);
+                Console.WriteLine(S);
+            }
+        }
+    }
+}
---
+++ Server/Server1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipEnd = new IPEndPoint(IPAddress.Any, 8000);
+            Socket Server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Server.Bind(ipEnd);
+            Server.Listen(5);
+            Socket newServer = Server.Accept();
+            string path = args[0];
+            Console.WriteLine(path);
+            FileStream fileStream = new FileStream(path, FileMode.Open);
+            StreamReader streamReader = new StreamReader(fileStream);
+            byte[] byte1 = BitConverter.GetBytes(path.Length);
+            byte[] byte2 = Encoding.ASCII.GetBytes(path);
+            Console.WriteLine("Ahmed_Hamed_Mohamed");
+            Console.WriteLine("1");
+            Console.WriteLine("CS");
+            while (streamReader.Peek() != -1)
+            {
+                string line = streamReader.ReadLine();
+                byte[] bytem = new byte[1024];
+                byte[] byted = Encoding.ASCII.GetBytes(line);
+                byte1.CopyTo(bytem, 0);
+                byte2.CopyTo(bytem, 4);
+                byted.CopyTo(bytem, 12);
+                newServer.Send(bytem);
+            }
+            byte[] t = new byte[1024];
+            byte[] comma = Encoding.ASCII.GetBytes(",");
+            byte1.CopyTo(t, 0);
+            byte2.CopyTo(t, 4);
+            comma.CopyTo(t, 4 + path.Length);
+            newServer.Send(t);
+            newServer.Shutdown(SocketShutdown.Both);
+            newServer.Close();
+        }
+    }
+}
+
